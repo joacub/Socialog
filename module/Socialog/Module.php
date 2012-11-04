5b07ef3a751df862a7a0f768a510a4613151eca0
Roy van Kaathoven <royvankaathoven@hotmail.com>
2012-11-04 13:50:38 +0100

    Added module interfaces

--- module/Socialog/Module.php
+++ module/Socialog/Module.php
@@ -2,13 +2,46 @@
 
 namespace Socialog;
 
-class Module
+use Zend\EventManager\EventInterface;
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
+use Zend\ModuleManager\Feature\ViewHelperProviderInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface,
+    ServiceProviderInterface,
+    ViewHelperProviderInterface,
+    BootstrapListenerInterface
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function onBootstrap(EventInterface $e)
+    {
+        $app = $e->getApplication();
+        $sm = $app->getServiceManager();
+        $sharedEventManager = $sm->get('SharedEventManager');
+
+        // Hook into comments
+        $sharedEventManager->attach('theme', 'post.post', function($e) use ($sm) {
+            $viewRenderer = $sm->get('ViewRenderer');
+            return $viewRenderer->partial('socialog/comment/post', $e->getParams());
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function getAutoloaderConfig()
     {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
+       return array(
+            AutoloaderFactory::STANDARD_AUTOLOADER => array(
+                StandardAutoloader::LOAD_NS => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
             ),
@@ -16,9 +49,7 @@ class Module
     }
 
     /**
-     * Service Configuration
-     *
-     * @return array
+     * {@inheritDoc}
      */
     public function getServiceConfig()
     {
@@ -26,7 +57,7 @@ class Module
     }
 
     /**
-     * @return array
+     * {@inheritDoc}
      */
     public function getConfig()
     {
@@ -34,9 +65,7 @@ class Module
     }
 
     /**
-     * View Helper Configuration
-     *
-     * @return array
+     * {@inheritDoc}
      */
     public function getViewHelperConfig()
     {
